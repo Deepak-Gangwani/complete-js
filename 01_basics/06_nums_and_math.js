45756e8eae06e395034b97fa1c5b017ed8932c22
Deepak-Gangwani <108722554+Deepak-Gangwani@users.noreply.github.com>
2024-01-22 11:37:24 +0000

    [numerical calculation]

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,2 +1,10 @@
 const score=400
-console.log(score)
+console.log(score);
+
+const balance=new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otheNumber=72367.7657
