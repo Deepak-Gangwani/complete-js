b5c5231b19d9bd2d3eb2735974be357c80cc1d5a
Deepak-Gangwani <108722554+Deepak-Gangwani@users.noreply.github.com>
2024-01-22 14:48:58 +0000

    [Adding Some More Files]

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primitive data type
+
+// 7types : String, Number, Boolean, null, undefined, symbol, BigInt
+const score=100
+const scorevalue=100.3
+
+
+const isLoggedIn = false
+const outsideTemp=null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId=Symbol('123')
+
+
+console.log(id===anotherId);
+
+
+const bigNumber=344763763737867367n
+
+
+// non-primitive data type
+// Array, Objects, Functions
+
+const heros=['shaktiman','naagraj','doga'];
+let myObj={
+    name:"hitesh",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
+
