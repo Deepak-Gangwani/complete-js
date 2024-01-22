62b04356349beedb8d7b053f9679275c5a6872d8
Deepak-Gangwani <108722554+Deepak-Gangwani@users.noreply.github.com>
2024-01-22 11:15:42 +0000

    Hello

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="deepak@gmail.com"
+var accountPassword="12345"
+accountCity="Jodhpur"
+
+let accountState;
+
+// accountId=5 //not allowed
+
+accountEmail="dg@dg.com"
+accountpassword="23232323"
+accountCity="Jodhpur"
+
+console.log(accountId)
+
+// We should not use var keyword variable beacause of local and global scope issues
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
